ba9b08fa4ecb5aab8f69919a8d9ef92995e22e17
Gtes <gates1993@gmail.com>
2020-04-29 00:01:36 +0300

    updates

--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -8,16 +8,11 @@ function isEmptyOrWhitespcesOrNull(str) {
 
 
 function getRandom(min = 0, max = 5) {
-    console.log(`
-    min = ${min} 
-    max = ${max}`)
-
     let number = Math.floor(Math.random() * (max - min + 1)) + min
-    console.log(number)
     return number;
 }
 
-function getAnswer(minNumber = 0, maxNumber = 5, attemptsLeft, totalPrize, posiblePrize = 100) {
+function getAnswer(minNumber = 0, maxNumber = 5, attemptsLeft, totalPrize, posiblePrize) {
     const userAnswer = prompt(`Choose a roulette pocket number from ${minNumber} to ${maxNumber}
     Attempts left: ${attemptsLeft}
     Total prize: ${totalPrize}$
@@ -44,12 +39,10 @@ const NUMBER_RANGE = {
 
 const MAX_PRIZE = 100;
 
-function game(gameNumber = 1, attemptsLeft = MAX_NUMBER_OF_ATTEMPTS, maxPrize = MAX_PRIZE, pastPrize = 0) {
+function game(gameNumber = 1, attemptsLeft = MAX_NUMBER_OF_ATTEMPTS, maxPrize = 100, pastPrize = 0) {
 
     let currentPrize = pastPrize;
     let curetntPosiblePrize = maxPrize;
-
-    console.log(`maxPrize = ${maxPrize}`)
 
     const randomNumber = getRandom(NUMBER_RANGE.min * gameNumber, NUMBER_RANGE.max * gameNumber);
 
@@ -71,10 +64,8 @@ function game(gameNumber = 1, attemptsLeft = MAX_NUMBER_OF_ATTEMPTS, maxPrize = 
 
             let finalPrize = currentPrize + lastPrize;
 
-            console.log(`lastPrize = ${lastPrize}`)
-
             if (confirm(`Congratulation, you won!   Your prize is: ${finalPrize} $. Do you want to continue?`)){
-                game(gameNumber + 1, MAX_NUMBER_OF_ATTEMPTS, MAX_PRIZE * gameNumber, currentPrize + lastPrize);
+                game(gameNumber + 1, MAX_NUMBER_OF_ATTEMPTS, MAX_PRIZE * gameNumber * 2, currentPrize + lastPrize);
             } else{
                 alert(`Thank you for your participation. Your prize is: ${finalPrize}$`)
             }
